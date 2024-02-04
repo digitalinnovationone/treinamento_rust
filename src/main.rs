c54e91a49064e25f46aa6fd7fa9effe218b374c2
Danilo Aparecido dos Santos <didox_59@yahoo.com.br>
2024-02-04 10:44:00 -0300

    add metodos nos structs

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,56 @@
-fn main() {
-    let x: String = String::from("Depurando código");
-    let x_modificada = mostra_na_tela_alterando(x);
-    print!("========================\n");
-    println!("Oláaaaa !!! - {}", x_modificada);
-    print!("========================\n");
+#[derive(Debug)]
+struct Cliente {
+    id: u32,
+    nome: String,
+    cpf: String,
+    salario: f32
 }
 
-fn mostra_na_tela_alterando(mut str: String) -> String {
-    str += " - alterando ...";
-    str
+impl Cliente {
+    fn cpf_valido(&self) -> bool {
+        if self.cpf.is_empty() {
+            return false;
+        }
+
+        true
+    }
+
+    fn adiciona_sobrenome(&mut self){
+        self.nome += " da Silva";
+    }
+
+    fn aumento(&mut self, valor:f32){
+        self.salario += valor;
+    }
+
+    fn aumento2(&self, valor:f32) -> f32{
+        self.salario + valor
+    }
+}
+
+fn main() {
+    let mut cliente = Cliente { 
+        id: 1,
+        nome: String::from("Leadro"),
+        cpf: String::from("222.555.553-00"),
+        salario: 5000.0
+    };
+
+    cliente.adiciona_sobrenome();
+
+    cliente.aumento(1000.0);
+
+    let valido = if cliente.cpf_valido()  {
+        "Verdadeiro"
+    } else {
+        "Falso"
+    };
+
+    println!(
+        "O CPF do cliente({}):  {} é {} \n{:#?}",
+        cliente.id,
+        cliente.nome,
+        valido,
+        cliente
+    );
 }
