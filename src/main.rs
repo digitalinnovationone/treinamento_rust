c975142f447d75fc8dd157a2500ae81320b649db
Danilo Aparecido dos Santos <didox_59@yahoo.com.br>
2024-02-06 15:52:04 -0300

    Pattern Matching

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,35 @@
-////////////////// Enum /////////////////////
+// Exemplo de Pattern Matching (Combinação de Padrões)
+// enum Comando {
+//     Iniciar(String),
+//     Parar,
+// }
+
+// fn executar_comando(comando: Comando) {
+//     match comando {
+//         Comando::Iniciar(mensagem) => println!("Iniciando: {}", mensagem),
+//         Comando::Parar => println!("Parando"),
+//     }
+// }
+
+// fn main() {
+//     let comando_iniciar = Comando::Iniciar(String::from("Motor"));
+//     let comando_iniciar2 = Comando::Iniciar(String::from("Combustão"));
+//     let comando_iniciar3 = Comando::Iniciar(String::from("Rolagem"));
+//     let comando_parar = Comando::Parar;
+
+//     executar_comando(comando_iniciar);
+//     executar_comando(comando_iniciar2);
+//     executar_comando(comando_iniciar3);
+//     executar_comando(comando_parar);
+// }
+
+
+
+
+
 // enum Tipo {
-//     Juridica,
-//     Fisica,
+//     Juridica(String),
+//     Fisica(String),
 // }
 
 // struct Pessoa {
@@ -13,17 +41,16 @@
 // fn main() {
 //     let daniel = Pessoa {
 //         nome: String::from("Daniel"),
-//         documento: String::from("99.999.999/9999-99"),
-//         tipo: Tipo::Juridica,
+//         documento: String::from("23323.3322332.323.332"),
+//         tipo: Tipo::Juridica(String::from("funcionario")),
 //     };
 
-//     // Pattern Matching
 //     match daniel.tipo {
-//         Tipo::Fisica => {
-//             println!("{} é uma pessoa física", daniel.nome)
+//         Tipo::Fisica(descricao) => {
+//             println!("{} é uma pessoa física({})", daniel.nome, descricao)
 //         },
-//         _ => {
-//             println!("{} é uma pessoa jurídica", daniel.nome)
+//         Tipo::Juridica(descricao) => {
+//             println!("{} é uma pessoa jurídica, {}", daniel.nome, descricao)
 //         },
 //     }
 // }
@@ -31,10 +58,70 @@
 
 
 
-#[derive(PartialEq)]
+// enum Tipo {
+//     Juridica(String),
+//     Fisica(i32),
+// }
+
+// struct Pessoa {
+//     nome: String,
+//     documento: String,
+//     tipo: Tipo,
+// }
+
+// fn main() {
+//     let daniel = Pessoa {
+//         nome: String::from("Daniel"),
+//         documento: String::from("23323.3322332.323.332"),
+//         tipo: Tipo::Juridica("xxxx".to_string()),
+//     };
+
+//     match daniel.tipo {
+//         Tipo::Fisica(valor) => {
+//             println!("{} é uma pessoa física({})", daniel.nome, valor)
+//         },
+//         Tipo::Juridica(valor) => {
+//             println!("{} é uma pessoa jurídica, {}", daniel.nome, valor)
+//         },
+//     }
+// }
+
+
+
+
+
+// enum Tipo {
+//     Juridica(String, i16),
+//     Fisica(i32, f64),
+// }
+
+// struct Pessoa {
+//     nome: String,
+//     documento: String,
+//     tipo: Tipo,
+// }
+
+// fn main() {
+//     let daniel = Pessoa {
+//         nome: String::from("Daniel"),
+//         documento: String::from("23323.3322332.323.332"),
+//         tipo: Tipo::Juridica("xxdsdsds".to_string(), 5),
+//     };
+
+//     match daniel.tipo {
+//         Tipo::Fisica(ref valor, ref valor_f) => {
+//             println!("{} é uma pessoa física({}), {}", daniel.nome, valor, valor_f)
+//         },
+//         Tipo::Juridica(ref valor, ref valor_f) => {
+//             println!("{} é uma pessoa jurídica, {} {}", daniel.nome, valor, valor_f)
+//         },
+//     }
+// }
+
+
 enum Tipo {
-    Juridica,
-    Fisica,
+    Juridica { mascara_cnpj: String, cnpj_governo: bool },
+    Fisica { mascara_cpf: String, cpf_governo: bool },
 }
 
 struct Pessoa {
@@ -43,18 +130,30 @@ struct Pessoa {
     tipo: Tipo,
 }
 
-
 fn main() {
     let daniel = Pessoa {
         nome: String::from("Daniel"),
         documento: String::from("23323.3322332.323.332"),
-        tipo: Tipo::Fisica,
+        tipo: Tipo::Fisica {
+            mascara_cpf: String::from("999.999.999-99"),
+            cpf_governo: true,
+        },
     };
 
-    if daniel.tipo == Tipo::Fisica {
-        println!("{} é uma pessoa física", daniel.nome);
-    } else {
-        println!("{} é uma pessoa jurídica", daniel.nome);
+    match daniel.tipo {
+        Tipo::Juridica { mascara_cnpj, cnpj_governo } => {
+            println!(
+                "{} é uma pessoa jurídica, Mascara CNPJ: {} - Governo: {}",
+                daniel.nome, mascara_cnpj, cnpj_governo
+            );
+        }
+        Tipo::Fisica { mascara_cpf, cpf_governo } => {
+            println!(
+                "{} é uma pessoa física, Mascara CPF: {}, documento: {} - Governo: {}",
+                daniel.nome, mascara_cpf, daniel.documento, cpf_governo
+            );
+        }
     }
 }
+
 
