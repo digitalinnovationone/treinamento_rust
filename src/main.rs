266009b0b575393d994f3855f3f849b932a121f1
Danilo Aparecido dos Santos <didox_59@yahoo.com.br>
2024-02-06 16:22:36 -0300

    Destructuring

--- src/main.rs
+++ src/main.rs
@@ -1,159 +1,121 @@
-// Exemplo de Pattern Matching (Combinação de Padrões)
-// enum Comando {
-//     Iniciar(String),
-//     Parar,
-// }
-
-// fn executar_comando(comando: Comando) {
-//     match comando {
-//         Comando::Iniciar(mensagem) => println!("Iniciando: {}", mensagem),
-//         Comando::Parar => println!("Parando"),
-//     }
-// }
-
-// fn main() {
-//     let comando_iniciar = Comando::Iniciar(String::from("Motor"));
-//     let comando_iniciar2 = Comando::Iniciar(String::from("Combustão"));
-//     let comando_iniciar3 = Comando::Iniciar(String::from("Rolagem"));
-//     let comando_parar = Comando::Parar;
-
-//     executar_comando(comando_iniciar);
-//     executar_comando(comando_iniciar2);
-//     executar_comando(comando_iniciar3);
-//     executar_comando(comando_parar);
-// }
-
-
-
-
-
-// enum Tipo {
-//     Juridica(String),
-//     Fisica(String),
-// }
+// Exemplo de Destructuring (Desestruturação)
 
 // struct Pessoa {
 //     nome: String,
-//     documento: String,
-//     tipo: Tipo,
+//     idade: u8,
 // }
 
 // fn main() {
-//     let daniel = Pessoa {
-//         nome: String::from("Daniel"),
-//         documento: String::from("23323.3322332.323.332"),
-//         tipo: Tipo::Juridica(String::from("funcionario")),
+//     let pessoa = Pessoa {
+//         nome: String::from("João"),
+//         idade: 30,
 //     };
 
-//     match daniel.tipo {
-//         Tipo::Fisica(descricao) => {
-//             println!("{} é uma pessoa física({})", daniel.nome, descricao)
-//         },
-//         Tipo::Juridica(descricao) => {
-//             println!("{} é uma pessoa jurídica, {}", daniel.nome, descricao)
-//         },
+//     // Desestruturação de uma struct
+//     let Pessoa { nome, idade } = pessoa;
+
+//     println!("Nome: {}, Idade: {}", nome, idade);
+// }
+
+
+// fn main() {
+//     let arr = [1, 2, 3, 4, 5];
+
+//     // Desestruturação de um array
+//     let [primeiro, segundo, ..] = arr;
+
+//     println!("Primeiro: {}, Segundo: {}", primeiro, segundo);
+// }
+
+
+// fn main() {
+//     let tupla: (&str, i32, f32) = ("João", 2021, 5.6);
+
+
+//     // Desestruturação de uma tupla
+//     let (linguagem, ano, ..) = tupla;
+
+//     let charsss = linguagem.chars();
+//     let quantidade = charsss.count();
+
+
+//     println!("Linguagem: {}, Ano: {}", linguagem, ano);
+// }
+
+
+// enum Mensagem {
+//     Enviar { id: u32, texto: String },
+// }
+
+// fn main() {
+//     let msg = Mensagem::Enviar { id: 1, texto: String::from("Olá, Rust!") };
+
+//     match msg {
+//         Mensagem::Enviar { id, texto } => {
+//             println!("ID: {}, Texto: {}", id, texto);
+//         }
 //     }
 // }
 
 
-
-
-// enum Tipo {
-//     Juridica(String),
-//     Fisica(i32),
+// struct Ponto {
+//     x: i32,
+//     y: i32,
 // }
+
+// fn main() {
+//     let ponto = Ponto { x: 10, y: 20 };
+
+//     // Desestruturação de uma struct
+//     let Ponto { x, y } = ponto;
+
+//     println!("x: {}, y: {}", x, y);
+// }
+
+
 
 // struct Pessoa {
 //     nome: String,
-//     documento: String,
-//     tipo: Tipo,
+//     sobrenome: String,
+//     idade: u8,
 // }
 
 // fn main() {
-//     let daniel = Pessoa {
-//         nome: String::from("Daniel"),
-//         documento: String::from("23323.3322332.323.332"),
-//         tipo: Tipo::Juridica("xxxx".to_string()),
+//     let pessoa = Pessoa {
+//         nome: String::from("João"),
+//         sobrenome: "Teste".to_string(),
+//         idade: 30,
 //     };
 
-//     match daniel.tipo {
-//         Tipo::Fisica(valor) => {
-//             println!("{} é uma pessoa física({})", daniel.nome, valor)
-//         },
-//         Tipo::Juridica(valor) => {
-//             println!("{} é uma pessoa jurídica, {}", daniel.nome, valor)
-//         },
-//     }
+//     // Desestruturação de uma struct
+//     let pessoa2 = Pessoa{ nome: "Silva", ... } = pessoa;
+
+//     println!("Pessoa Nova: {}", pessoa2.nome);
 // }
 
 
-
-
-
-// enum Tipo {
-//     Juridica(String, i16),
-//     Fisica(i32, f64),
-// }
-
-// struct Pessoa {
-//     nome: String,
-//     documento: String,
-//     tipo: Tipo,
-// }
-
-// fn main() {
-//     let daniel = Pessoa {
-//         nome: String::from("Daniel"),
-//         documento: String::from("23323.3322332.323.332"),
-//         tipo: Tipo::Juridica("xxdsdsds".to_string(), 5),
-//     };
-
-//     match daniel.tipo {
-//         Tipo::Fisica(ref valor, ref valor_f) => {
-//             println!("{} é uma pessoa física({}), {}", daniel.nome, valor, valor_f)
-//         },
-//         Tipo::Juridica(ref valor, ref valor_f) => {
-//             println!("{} é uma pessoa jurídica, {} {}", daniel.nome, valor, valor_f)
-//         },
-//     }
-// }
-
-
-enum Tipo {
-    Juridica { mascara_cnpj: String, cnpj_governo: bool },
-    Fisica { mascara_cpf: String, cpf_governo: bool },
-}
-
+#[derive(Debug)] // Deriva o traço Debug para permitir a impressão de debug
 struct Pessoa {
     nome: String,
-    documento: String,
-    tipo: Tipo,
+    sobrenome: String,
+    idade: u8,
 }
 
 fn main() {
-    let daniel = Pessoa {
-        nome: String::from("Daniel"),
-        documento: String::from("23323.3322332.323.332"),
-        tipo: Tipo::Fisica {
-            mascara_cpf: String::from("999.999.999-99"),
-            cpf_governo: true,
-        },
+    let pessoa = Pessoa {
+        nome: String::from("João"),
+        sobrenome: String::from("Teste"),
+        idade: 30,
     };
 
-    match daniel.tipo {
-        Tipo::Juridica { mascara_cnpj, cnpj_governo } => {
-            println!(
-                "{} é uma pessoa jurídica, Mascara CNPJ: {} - Governo: {}",
-                daniel.nome, mascara_cnpj, cnpj_governo
-            );
-        }
-        Tipo::Fisica { mascara_cpf, cpf_governo } => {
-            println!(
-                "{} é uma pessoa física, Mascara CPF: {}, documento: {} - Governo: {}",
-                daniel.nome, mascara_cpf, daniel.documento, cpf_governo
-            );
-        }
-    }
+    // Desestruturação de uma struct
+    let Pessoa { nome, sobrenome, idade } = &pessoa;
+
+    let pessoa2 = Pessoa {
+        nome: nome.to_owned() + " Silva",
+        sobrenome: sobrenome.to_owned(), // Faz uma cópia do valor
+        idade: *idade, // (desreferenciação) não cria uma cópia do valor; em vez disso, ela apenas fornece acesso ao valor original que a referência aponta.
+    };
+
+    println!("Pessoa Nova: {:#?}", pessoa2);
 }
-
-
